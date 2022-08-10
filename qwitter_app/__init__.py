b21fec1a27007614e0003ab9a8000c20e1e92767
NikitaKurabtsev <nick3500hunna@gmail.com>
2022-08-10 20:33:27 +0300

    reafactor signals

---
+++ qwitter_app/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'qwitter_app.apps.QwitterAppConfig'
--- qwitter_app/models.py
+++ qwitter_app/models.py
@@ -1,8 +1,5 @@
-from operator import mod
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -13,12 +10,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-        user_profile.follows.add(instance.profile)
-        user_profile.save()
