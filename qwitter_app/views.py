d8d03fe685c8ba480e18fd3ba5c93304addc6c5f
NikitaKurabtsev <nick3500hunna@gmail.com>
2022-08-11 19:32:25 +0300

    some controllers and templates

--- qwitter_app/views.py
+++ qwitter_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from qwitter_app.models import Profile
+
+
+def dashboard(request):
+    return render(request, 'base.html')
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    template_path = 'qwitter_app/profile_list.html'
+    context = {'profiles': profiles}
+    return render(request, template_path, context)
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    template_path = 'qwitter_app/profile.html'
+    context = {'profile': profile}
+    return render(request, template_path, context)
